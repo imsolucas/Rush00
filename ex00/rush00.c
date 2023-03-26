8c1d2c376c5d7bf45371c55198ed02eb80fa8665
Lucas <denglucasyijin@gmail.com>
2023-03-26 14:36:03 +0800

    re-edit rush00

--- ex00/rush00.c
+++ ex00/rush00.c
@@ -1,45 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: djin <djin@42.fr>                          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/26 10:58:28 by djin              #+#    #+#             */
+/*   Updated: 2023/03/26 14:31:58 by djin             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void ft_putchar(char c);
+void	ft_putchar(char c);
 
-void rush(void)	{
-	int y = 5;
-	int x = 5;
-	char a = 111;
-	char b = 45;
-	char c = 124;
-	int i = 1;
-	int j = 1;
+void	rush(void)
+{
+	int	y;
+	int	x;
+	int	i;
+	int	j;
+	char	a = 111;
+	char	b = 45;
+	char	c = 124;
 
-	while(i <= y)
+	x = 5;
+	y = 5;
+	i = 1;
+	j = 1;
+	while (i <= y)
 	{	
 		j = 1;
-		while(j <= x) 
+		while (j <= x)
 		{					
-			if(i == 1 || j == 1 || i == y || j == x)	
+			if (i == 1 || j == 1 || i == y || j == x)
 			{
-			if((i == 1 || i == y) && (j == 1 || j == x))
-			{
-				ft_putchar(a);	
-			}
-			else if((i > 1 || i < y) && (j == 1 || j == x))	
-			{
-				ft_putchar(c);
+				if ((i == 1 || i == y) && (j == 1 || j == x))
+				{
+					ft_putchar(a);
+				}
+				else if ((i > 1 || i < y) && (j == 1 || j == x))
+				{
+					ft_putchar(c);
+				}
+				else
+				{
+					ft_putchar(b);
+				}
 			}
 			else
-		   	{
-			   ft_putchar(b);
+			{
+				ft_putchar(' ');
 			}
-			}
-		else 
-		{
-			ft_putchar(' ');
-		}	
 		j++;
 		}
-		
 		ft_putchar('\n');
 		i++;
 	}
 }
-
